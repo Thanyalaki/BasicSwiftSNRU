1cf48ab6e4ea35e23c0dbf936cbc200f5b399999
nook <Thayalak.nk59@snru.ac.th>
2019-03-18 15:20:27 +0700

    array ok

--- DataTypeOfVariable.playground/Contents.swift
+++ DataTypeOfVariable.playground/Contents.swift
@@ -6,5 +6,30 @@ var myName = "Doramon" //นี่คือการประกาศ ที่
 var mySurname: String = "AAAAAA" //นี้คือการปรพกาศแบบกำหนด Datatype ตั้งแต่เกิด
 
 
+//การแสดงผลบน console
+print("THis is Console")
+print("myName = \(myName)")
 
+//lnteger Type หรือ จำนวนที่ไม่มีเศษ
+var number1 = 100
+var number2:  Int = 200
 
+//Double Type หรือ จำนวนที่มีเศษ
+var number3 = 3.1416
+var number4: Double = 10.234
+
+//Boolean Type คือจำนวนที่มีแค่ค่า true,false
+var status = true
+var status2:Bool = false
+
+//Array Type
+var myFriends = ["Doramon","Nopita","Sunako","Chisuka"]
+print("myFriends ที่มี index =1 มีค่าเท่ากับ => \(myFriends)")
+
+//การเพิ่มสมาชิก  array
+myFriends.append("nook")
+print(myFriends)
+
+//การลบสมาชิก ของ Array
+myFriends.remove(at: 1)
+print(myFriends)
