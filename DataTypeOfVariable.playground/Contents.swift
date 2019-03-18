416dccf71a504d14c9d504da716d1d7136ad8378
nook <Thayalak.nk59@snru.ac.th>
2019-03-18 14:28:13 +0700

    Start DataType

---
+++ DataTypeOfVariable.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+//Sting Type ตัวแปรที่เป็นอักษร
+
+var myName = "Doramon" //นี่คือการประกาศ ที่อาศัยนิดของ value บอก Datatype
+var mySurname: String = "AAAAAA" //นี้คือการปรพกาศแบบกำหนด Datatype ตั้งแต่เกิด
+
+
+
+
---
+++ VarialbleAndContant.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+//Explicit Variable
+//การประกาศตัวแปร
+
+var myName = "Nook"
+myName = "Doramon"
+
+//Explicit Cconstant
+let MyFriend = "Nopita"
+//MyFriend = "Nopita" Error Because myFriehd myFriend is let
+
